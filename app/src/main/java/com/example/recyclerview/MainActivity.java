fd2c28c28be33f4c10f4f3274eb1322411fa2b74
pawan_bharati <pawanbharati90@gmail.com>
2019-11-25 12:12:32 +0545

    recycler view complete

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recyclerView);
+        List<Contacts>contactsList=new ArrayList<>();
+        contactsList.add(new Contacts("index","9840096937",R.drawable.index));
+        contactsList.add(new Contacts("index1","9840096938",R.drawable.index1));
+        contactsList.add(new Contacts("index2","9840096939",R.drawable.index2));
+
+        ContactsAdapter contactsAdapter=new ContactsAdapter(this,contactsList);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
